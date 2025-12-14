be8e2ec0f15d5b45782eb3adb4182d41153fe89d
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2025-12-14 17:34:23 +0000

    Revert "BACKPORT: Implement SuSFS v1.5.12"
    This reverts commit 5b05518e2ac4412ac632cb00d5e23bb6821aba7f.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1479,15 +1479,8 @@ struct task_struct {
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
 	ANDROID_KABI_RESERVE(5);
-#if defined(CONFIG_KSU_SUSFS)
-	ANDROID_KABI_USE(6, u64 susfs_task_state);
-#else	
 	ANDROID_KABI_RESERVE(6);
-#endif // #if defined(CONFIG_KSU_SUSFS)	
 #else
-#ifdef CONFIG_KSU_SUSFS
-	u64 susfs_task_state;
-#endif
 	struct mutex			futex_exit_mutex;
 #endif
 
--- kernel/sys.c
+++ kernel/sys.c
@@ -1251,9 +1251,6 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1293,9 +1290,6 @@ SYSCALL_DEFINE1(uname, struct old_utsname __user *, name)
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	up_read(&uts_sem);
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
-#endif
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
